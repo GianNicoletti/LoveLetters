f747bf66d075a290e9da58249202a8aaf0516764
HydratedPhoenix <63128242+HydratedPhoenix@users.noreply.github.com>
2020-11-12 00:54:30 -0300

    Update App.java

--- src/app/App.java
+++ src/app/App.java
@@ -7,14 +7,17 @@ import sala.Sala;
 public class App {
 	public static void main(String[] args) {
 		// Jugador carlos = new Jugador("Carlos-1");
-
-		// LoginWindowOffline win=new LoginWindowOffline();
-		// win.setVisible(true);
-
-		Sala sala = new Sala(new Jugador("Carlos-1"), 3);
-		sala.agregarJugador(new Jugador("Roberto-2"));
-		sala.agregarJugador(new Jugador("Hernesto-3"));
-		sala.agregarJugador(new Jugador("Ricardo-4"));
+		
+		LoginWindowOffline win=new LoginWindowOffline();
+		win.setVisible(true);
+		Sala sala;
+		sala=win.getSala();
 		sala.empezarJuego();
+
+		//Sala sala = new Sala(new Jugador("Carlos-1"), 3);
+		//sala.agregarJugador(new Jugador("Roberto-2"));
+		//sala.agregarJugador(new Jugador("Hernesto-3"));
+		//sala.agregarJugador(new Jugador("Ricardo-4"));
+		//sala.empezarJuego();
 	}
 }
